52cf82f93c1d2786704d12fd56ed526b4005b677
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-06-19 00:27:49 +0300

    Auth pages

--- portal/src/pages/login.js
+++ portal/src/pages/login.js
@@ -1,7 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
+import { Navigate } from "react-router-dom"
+
+import { useAuth } from "../components/auth-provider"
 
 function Login() {
-  return <div>Login</div>
+  const { isLogged, login } = useAuth()
+  const [errors, setErrors] = useState()
+
+  function onSubmit(ev) {
+    ev.preventDefault()
+
+    const form = ev.currentTarget
+    const email = form.email.value
+    const password = form.password.value
+
+    login({ email, password }).catch(setErrors)
+  }
+
+  if (isLogged) {
+    return <Navigate to="/" />
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="email">email</label>
+      <input name="email" type="email" required />
+      {errors?.email && <span>{errors.email.message}</span>}
+      <br />
+
+      <label htmlFor="password">password</label>
+      <input name="password" type="password" required />
+      {errors?.password && <span>{errors.password.message}</span>}
+      <br />
+
+      <button type="submit">login</button>
+      {errors && !errors.email && !errors.password ? errors.message : null}
+    </form>
+  )
 }
 
 export default Login
--- portal/src/pages/logout.js
+++ portal/src/pages/logout.js
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { Navigate } from "react-router-dom"
+
+import { useAuth } from "../components/auth-provider"
+import Loading from "../components/loading"
 
 function Logout() {
-  return <div>Logout</div>
+  const { isLogged, logout } = useAuth()
+
+  useEffect(() => {
+    if (isLogged) {
+      logout()
+    }
+  }, [isLogged, logout])
+
+  if (isLogged) {
+    return <Loading />
+  }
+
+  return <Navigate replace to="/" />
 }
 
 export default Logout
