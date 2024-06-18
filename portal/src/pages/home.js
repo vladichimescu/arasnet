7874186e3f661085ff17129eab59ca91ea378cba
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-06-18 13:31:24 +0300

    Dummy pages

---
+++ portal/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Navigate } from "react-router-dom"
+
+import { useAuth } from "../components/auth-provider"
+
+function Home() {
+  const { isLogged } = useAuth()
+
+  return <Navigate replace to={isLogged ? "dashboard" : "landing"} />
+}
+
+export default Home
