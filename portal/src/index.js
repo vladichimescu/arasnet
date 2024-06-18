7e68a8dc7547a8b629b1bfdb4a1bc140fe04ff2e
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-06-18 13:41:05 +0300

    Portal basic structure

--- portal/src/index.js
+++ portal/src/index.js
@@ -1,12 +1,50 @@
-import React from "react"
+import React, { Fragment, Suspense, lazy } from "react"
 import ReactDOM from "react-dom/client"
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom"
 
-import App from "./app"
+import AuthProvider from "./components/auth-provider"
+import ErrorBoundary from "./components/error-boundary"
+import Loading from "./components/loading"
 import "./index.css"
+import NotFound from "./pages/not-found"
+
+const Dashboard = lazy(() => import("./pages/dashboard"))
+const Landing = lazy(() => import("./pages/landing"))
+const Login = lazy(() => import("./pages/login"))
+const Logout = lazy(() => import("./pages/logout"))
+const Home = lazy(() => import("./pages/home"))
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Fragment>
+      <Route path="" element={<Home />} />
+
+      <Route path="landing" element={<Landing />} />
+
+      <Route path="dashboard" element={<Dashboard />} />
+
+      <Route path="logout" element={<Logout />} />
+      <Route path="login" element={<Login />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Fragment>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <Suspense fallback={<Loading />}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </Suspense>
+    </ErrorBoundary>
   </React.StrictMode>
 )
