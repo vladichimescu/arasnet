503039e4a78e996f9f74a1eb0509e663e29ae7c5
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-06-21 01:29:38 +0300

    Props check

--- server/libs/mandatory-props-check.js
+++ server/libs/mandatory-props-check.js
@@ -2,7 +2,7 @@ function mandatoryPropsCheck(obj = {}, mandatoryProps = []) {
   const errors = {}
 
   mandatoryProps.forEach((iProp) => {
-    if (obj[iProp] === undefined || obj[iProp] === "") {
+    if (obj[iProp] === undefined || obj[iProp] === null || obj[iProp] === "") {
       errors[iProp] = {
         code: `${iProp}_required`,
         message: `${iProp} required`,
