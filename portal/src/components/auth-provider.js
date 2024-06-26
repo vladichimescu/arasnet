6c85aef9d49f35ac347b55e438c4b756a761ecd9
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-06-26 23:29:30 +0300

    Cleanup

--- portal/src/components/auth-provider.js
+++ portal/src/components/auth-provider.js
@@ -1,7 +1,6 @@
 import React, { createContext, useCallback, useContext, useState } from "react"
 
 import AuthApi from "../apis/auth-api"
-import EmployeesApi from "../apis/employees-api"
 import AuthService from "../services/auth-service"
 
 const Context = createContext()
@@ -24,20 +23,12 @@ function AuthProvider(props) {
     setIsLogged(false)
   }, [])
 
-  const register = useCallback(async (account) => {
-    const user = await EmployeesApi.create(account)
-    login(account)
-
-    return user
-  }, [])
-
   return (
     <Context.Provider
       value={{
         isLogged,
         login,
         logout,
-        register,
       }}
       {...props}
     />
