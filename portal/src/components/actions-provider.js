d718260205bb3b4a10051f1783706d780c04a8cd
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-06-26 23:25:04 +0300

    Actions provider

---
+++ portal/src/components/actions-provider.js
@@ -0,0 +1,62 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react"
+
+const Context = createContext()
+
+function ActionsProvider(props) {
+  const [actionRegistry, setActionRegistry] = useState({})
+  const [actions, setActions] = useState([])
+
+  useEffect(() => {
+    setActions(Object.values(actionRegistry))
+  }, [actionRegistry])
+
+  const addAction = useCallback((handler, action) => {
+    const actionId = crypto.randomUUID()
+
+    setActionRegistry((state) => ({
+      ...state,
+      [actionId]: {
+        action,
+        handler,
+      },
+    }))
+
+    return actionId
+  }, [])
+
+  const removeAction = useCallback((actionId) => {
+    setActionRegistry((state) => {
+      delete state[actionId]
+
+      return { ...state }
+    })
+  }, [])
+
+  const clearActions = useCallback(() => {
+    setActionRegistry((state) => {
+      state.clear()
+      return { ...state }
+    })
+  }, [])
+
+  return (
+    <Context.Provider
+      value={{
+        actions,
+        addAction,
+        removeAction,
+        clearActions,
+      }}
+      {...props}
+    />
+  )
+}
+
+export default ActionsProvider
+export const useActions = () => useContext(Context)
