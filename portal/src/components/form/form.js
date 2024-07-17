7c24e62c310e2e9f01286f877d17c72076ee64fb
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-07-17 10:41:12 +0300

    Form logic, structure and flow

---
+++ portal/src/components/form/form.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from "react"
+import PhoneInput from "react-phone-input-2"
+
+import styles from "./form.module.scss"
+
+function Form({
+  className,
+  heading,
+  content,
+  footer,
+  onSubmit,
+  onCancel,
+  inputs,
+}) {
+  const focusRef = useRef()
+
+  const [errors, setErrors] = useState()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      await onSubmit(Object.fromEntries(new FormData(event.target)))
+    } catch (err) {
+      setErrors(err)
+    }
+  }
+
+  useEffect(() => {
+    focusRef.current?.focus()
+  }, [])
+
+  return (
+    <form onSubmit={handleSubmit} className={`${styles.form} ${className}`}>
+      {heading}
+
+      {inputs?.map(({ type, label, name, list, ...props }) => (
+        <fieldset key={name}>
+          <label htmlFor={name}>{label}</label>
+          {type === "phone-input" ? (
+            <PhoneInput
+              {...props}
+              inputProps={{
+                id: name,
+                name,
+                required: props.required,
+              }}
+            />
+          ) : type === "select" ? (
+            <select name={name} {...props}>
+              {list.map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <input id={name} name={name} type={type} {...props} />
+          )}
+
+          <small>{errors?.[name]?.message}</small>
+        </fieldset>
+      ))}
+
+      {content}
+
+      <fieldset style={{ marginBottom: 0 }}>
+        {onSubmit ? <button type="submit">Submit</button> : null}
+
+        <small>{errors?.message}</small>
+      </fieldset>
+
+      {onCancel ? (
+        <button
+          ref={focusRef}
+          type="button"
+          className="button-outline"
+          value="cancel"
+          onClick={() => onCancel()}
+        >
+          Cancel
+        </button>
+      ) : null}
+
+      {footer}
+    </form>
+  )
+}
+
+export default Form
