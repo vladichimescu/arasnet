e1e5039ce49c475931ed4dc86e8fd3998faa7128
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2024-08-16 21:25:14 +0300

    Crypto functions

--- libs/functions/index.js
+++ libs/functions/index.js
@@ -1,2 +1,3 @@
 export { default as validateRequiredFields } from "./src/validate-required-fields.js"
 export { default as toCamelCase } from "./src/to-camel-case.js"
+export * from "./src/crypto.js"
---
+++ libs/functions/src/crypto.js
@@ -0,0 +1,77 @@
+const algorithm = "AES-GCM"
+
+const masterKey = await crypto.subtle.digest(
+  "SHA-256",
+  new TextEncoder().encode(process.env.SECRET)
+)
+
+const criptoKey = await crypto.subtle.importKey(
+  "raw",
+  masterKey,
+  { name: algorithm },
+  false,
+  ["encrypt", "decrypt"]
+)
+
+async function encrypt(data) {
+  const iv = new Uint8Array(12)
+  crypto.getRandomValues(iv)
+
+  const encodedData = new TextEncoder().encode(data)
+
+  const encryptedData = await crypto.subtle.encrypt(
+    {
+      name: algorithm,
+      iv,
+    },
+    criptoKey,
+    encodedData
+  )
+
+  const ivString = btoa(String.fromCharCode(...iv))
+
+  const encryptedDataString = btoa(
+    String.fromCharCode(...new Uint8Array(encryptedData))
+  )
+
+  return `${ivString}${encryptedDataString}`
+}
+
+async function decrypt(data) {
+  const ivString = data.slice(0, 16)
+
+  const encryptedDataString = data.slice(16)
+
+  const iv = new Uint8Array(
+    atob(ivString)
+      .split("")
+      .map((c) => c.charCodeAt(0))
+  )
+
+  const encryptedData = new Uint8Array(
+    atob(encryptedDataString)
+      .split("")
+      .map((c) => c.charCodeAt(0))
+  )
+
+  const encodedData = await crypto.subtle.decrypt(
+    {
+      name: algorithm,
+      iv,
+    },
+    criptoKey,
+    encryptedData
+  )
+
+  return new TextDecoder().decode(encodedData)
+}
+
+async function verify(encryptedData, originalData) {
+  try {
+    return originalData === (await decrypt(encryptedData))
+  } catch {
+    return false
+  }
+}
+
+export { encrypt, decrypt, verify }
