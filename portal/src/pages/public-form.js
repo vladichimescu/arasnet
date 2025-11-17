fd5fce6997c5f52131a75abbb7cfd0cae14c6663
Vlad Ichimescu <vlad.ichimescu@gmail.com>
2025-11-17 10:50:57 +0200

    Enhance public form functionality

--- portal/src/pages/public-form.js
+++ portal/src/pages/public-form.js
@@ -1,34 +1,68 @@
-import React from "react"
+import React, { Fragment, useEffect } from "react"
 
-import { i18n } from "@arasnet/i18n"
+import { Trans, i18n } from "@arasnet/i18n"
 import { services } from "@arasnet/types"
 
+import PartnerApi from "../apis/partner-api"
+import TestingApi from "../apis/testing-api"
+import { useAuth } from "../components/auth-provider"
 import Form from "../components/form/form"
+import PartnerForm from "../components/partner-form"
+import TestingForm from "../components/testing-form"
 
 const containerStyle = {
-  maxWidth: "500px",
+  alignSelf: "center",
   marginLeft: "auto",
   marginRight: "auto",
+  width: "min(90vw, 400px)",
 }
 
 function PublicForm() {
-  const [hasAgreed, setHasAgreed] = React.useState()
-  const [formType, setFormType] = React.useState()
+  const { login, logout } = useAuth()
+
+  const [hasAgreed, setHasAgreed] = React.useState(null)
+  const [formType, setFormType] = React.useState(null)
+  const [isSubmitted, setIsSubmitted] = React.useState(false)
+
+  useEffect(() => {
+    login({
+      email: process.env.REACT_APP_PUBLIC_FORM_EMAIL,
+      password: process.env.REACT_APP_PUBLIC_FORM_PASSWORD,
+    })
+
+    return () => {
+      logout()
+    }
+  }, [login, logout])
+
+  if (isSubmitted) {
+    return <FormSuccess />
+  }
 
   if (formType) {
     switch (formType) {
       case services.TESTING:
-        return <FormTesting />
+        return (
+          <FormTesting
+            onSubmit={() => setIsSubmitted(true)}
+            onCancel={() => setFormType(null)}
+          />
+        )
       case services.PREP:
         return <FormPrep />
       case services.PARTNER:
-        return <FormPartner />
+        return (
+          <FormPartner
+            onSubmit={() => setIsSubmitted(true)}
+            onCancel={() => setFormType(null)}
+          />
+        )
       default:
         throw i18n.t("generic.error.service_invalid.message")
     }
   }
 
-  if (hasAgreed === undefined) {
+  if (hasAgreed === null) {
     return <AvailabilityForm onSubmit={(data) => setHasAgreed(data)} />
   }
 
@@ -36,19 +70,24 @@ function PublicForm() {
     return <FormDenied />
   }
 
-  return <FormTypes onSubmit={(data) => setFormType(data)} />
+  return (
+    <FormTypes
+      onSubmit={(data) => setFormType(data)}
+      onCancel={() => setHasAgreed(null)}
+    />
+  )
 }
 
-function AvailabilityForm({ onSubmit }) {
+function AvailabilityForm({ onSubmit: handleSubmit }) {
   return (
     <Form
       style={containerStyle}
       onSubmit={async () => {
-        onSubmit(true)
+        handleSubmit(true)
       }}
       submitLabel={i18n.t("generic.boolean.yes")}
       onCancel={() => {
-        onSubmit(false)
+        handleSubmit(false)
       }}
       cancelLabel={i18n.t("generic.boolean.no")}
       heading={<h4>{i18n.t("page.publicForm.AvailabilityForm.heading")}</h4>}
@@ -59,25 +98,13 @@ function AvailabilityForm({ onSubmit }) {
   )
 }
 
-function FormDenied() {
-  return (
-    <div
-      style={{
-        ...containerStyle,
-        whiteSpace: "pre-line",
-      }}
-    >
-      {i18n.t("page.publicForm.FormDenied.content")}
-    </div>
-  )
-}
-
-function FormTypes({ onSubmit }) {
+function FormTypes({ onSubmit: handleSubmit, onCancel: handleCancel }) {
   const serviceList = Object.values(services).map((service) => [
     service,
     {
       label: i18n.t(`entity.${service}.denomination`),
       value: service,
+      disabled: service === services.PREP,
     },
   ])
 
@@ -85,9 +112,11 @@ function FormTypes({ onSubmit }) {
     <Form
       style={containerStyle}
       onSubmit={async (data) => {
-        onSubmit(data.formType)
+        handleSubmit(data.formType)
       }}
       submitLabel={i18n.t("generic.action.continue")}
+      onCancel={handleCancel}
+      cancelLabel={i18n.t("generic.action.back")}
       inputs={[
         {
           type: "select",
@@ -98,20 +127,108 @@ function FormTypes({ onSubmit }) {
           defaultValue: services.TESTING,
         },
       ]}
+      content={
+        <small>
+          <Trans
+            i18nKey="page.publicForm.GDPR"
+            components={{
+              gdprLink: (
+                // eslint-disable-next-line jsx-a11y/anchor-has-content
+                <a
+                  href="/privacy-policy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+              ),
+            }}
+          />
+        </small>
+      }
     />
   )
 }
 
-function FormTesting() {
-  return <div>Testing Form</div>
+function FormTesting({ onSubmit: handleSubmit, onCancel: handleCancel }) {
+  return (
+    <TestingForm
+      style={containerStyle}
+      onSubmit={async (data) => {
+        await TestingApi.create(data)
+
+        handleSubmit()
+      }}
+      onCancel={handleCancel}
+      cancelLabel={i18n.t("generic.action.back")}
+    />
+  )
 }
 
 function FormPrep() {
   return <div>Prep Form</div>
 }
 
-function FormPartner() {
-  return <div>Partner Form</div>
+function FormPartner({ onSubmit: handleSubmit, onCancel: handleCancel }) {
+  return (
+    <PartnerForm
+      style={containerStyle}
+      onSubmit={async (data) => {
+        await PartnerApi.create(data)
+
+        handleSubmit()
+      }}
+      onCancel={handleCancel}
+      cancelLabel={i18n.t("generic.action.back")}
+    />
+  )
+}
+
+function FormDenied() {
+  const phoneNumber = "+40212100747"
+  const mapAddress = "PrEPpoint ARAS Bucuresti, București 030167"
+
+  const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(mapAddress)}`
+
+  return (
+    <Fragment>
+      <div
+        style={{
+          ...containerStyle,
+          whiteSpace: "pre-line",
+        }}
+      >
+        <h1>{i18n.t("page.publicForm.FormDenied.heading")}</h1>
+
+        <Trans
+          i18nKey="page.publicForm.FormDenied.content"
+          components={{
+            phoneLink: (
+              // eslint-disable-next-line jsx-a11y/anchor-has-content
+              <a
+                style={{
+                  whiteSpace: "nowrap",
+                }}
+                href={`tel:${phoneNumber}`}
+              />
+            ),
+            mapsLink: (
+              // eslint-disable-next-line jsx-a11y/anchor-has-content
+              <a href={mapsUrl} target="_blank" rel="noopener noreferrer" />
+            ),
+          }}
+        />
+      </div>
+    </Fragment>
+  )
+}
+
+function FormSuccess() {
+  return (
+    <div style={containerStyle}>
+      <h1>{i18n.t("page.publicForm.FormSuccess.heading")}</h1>
+
+      <p>{i18n.t("page.publicForm.FormSuccess.content")}</p>
+    </div>
+  )
 }
 
 export default PublicForm
